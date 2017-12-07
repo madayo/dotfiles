47c0c0c745449133d81eb142b4aa4d777af29006
madayo <madayodama@gmail.com>
2017-12-07 14:10:05 +0900

    全体的にUbuntu向けで再構築

--- initialize.sh
+++ initialize.sh
@@ -1,14 +1,55 @@
 #! /bin/bash -e
 
-source ~/dotfiles/bin/functions
+PROMPT=">   "
 
 # Ubuntu-only stuff. Abort if not Ubuntu.
-[[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || print_error 'Only ubuntu is enabled.'; exit 1
+if [[ ! "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]];then
+  print_error 'Only ubuntu is enabled.'
+  exit 1
+fi
+
+source ~/dotfiles/bin/functions
 
 # update
 sudo apt update
 sudo apt upgrade -y
 
-/bin/sh ~/dotfiles/link.sh
+# link
+/bin/bash ~/dotfiles/link.sh
+
+# set git author
+echo "----------------------------"
+echo "Would you like to set committer information in '~/.gitconfig.local'? (y/n)" 
+while read -p "$PROMPT" yn; do
+  case $yn in
+    'y' ) 
+      echo '[user.name]?'
+      read -p "$PROMPT" name
+      echo '[user.email]?'
+      read -p "$PROMPT" email
+      input="[user]
+  name  = $name
+  email = $email"
+      echo "$input"
+      echo "Is this all right? (y/n)"
+ 
+      while read -p "$PROMPT" yn; do
+        case $yn in
+          'y' ) 
+            do_backup_if_present "~/.gitconfig.local"
+            echo "$input" > ~/.gitconfig.local
+            break 2 ;;
+          'n' ) 
+            echo "Would you like to set committer information in '~/.gitconfig.local'? (y/n)";
+            break;
+            ;;
+        esac
+      done
+      ;;
+    'n' ) break ;;
+  esac
+done
+
+printf "\e[1;32mCompleted!\e[m\n"
 
 source ~/.bashrc
--- link.sh
+++ link.sh
@@ -1,16 +1,15 @@
 #! /bin/bash -e
 
+source ~/dotfiles/bin/functions
+
 # git管理下にある dotfile のみ抽出
 for f in `git ls-files | sed -s 's/\/.*//g' | grep '^\.' | uniq`
 do
   # リンクを貼らないものたち
-  [[ "$f" =~ ".git" ]] && continue
+  [[ $f =~ ".git" ]] && continue
 
-  # 既にファイルが存在し、かつシンボリックリンクではない場合はバックアップ
-  if [[ -e ~/"$f" ]] && [[ ! -L ~/"$f" ]]; then
-    backup_time=`date +"%Y%m%d_%H%M%S"`
-    mv ~/"$f" ~/"${f}.${backup_time}"
-    print_info "[info] present ${f} is moved to ${f}.${backup_time}"
-  fi
-  ln -nfs ~/dotfiles/"$f" ~/"$f"
+  ln_dotfile ~/dotfiles/$f ~/$f
 done
+
+# リンク貼るが、ほかとディレクトリ構成が異なるものは個別に対応する
+ln_dotfile ~/dotfiles/.git_global/.gitconfig ~/.gitconfig
