bfe469c12f6760f43bd7ab9794d90b54ab27a4a4
madayo <madayodama@gmail.com>
2018-01-15 10:02:03 +0900

    gitdiff. winmergeを止めて,vimdiffで比較する

--- bin/tmux/get_git_info.sh
+++ bin/tmux/get_git_info.sh
@@ -1,11 +1,14 @@
 #! /bin/bash -e
 
 function get_git_info {
-  cd `tmux display-message -p -F "#{pane_current_path}"`
+  # tmuxのバージョンによっては pane_current_path が使用できない
+  #path=`tmux display-message -p "#{pane_current_path}"`
+  # 基本的にタイトルにはパス情報を設定しているので、そちらから判定する
+  # tmux上でチルダがうまく変換できないようなので、eval echo を挟むことでフルパスに展開する
+  cd $(eval echo $(tmux display-message -p "#T"))
   local user_name=`git config --get user.name`
   local email_address=`git config --get user.email`
   local branch_name=`git branch | grep \*.* | sed -e 's/\*\ //'`
-
   local cmd=$(tmux display-message -p "#{pane_current_command}")
 
   local str=
