06ed75128c2a8d5e3f93f3b796bbdc1364a3f14f
madayo <madayodama@gmail.com>
2018-05-02 12:21:24 +0900

    tmux. git情報の表示シェルスクリプトの改良
    リポジトリ名もstatus lineに表示する

--- bin/tmux/get_git_info.sh
+++ bin/tmux/get_git_info.sh
@@ -12,11 +12,15 @@ function get_git_info {
   cd $current_dir
   local user_name=`git config --get user.name`
   local email_address=`git config --get user.email`
+  local repo_name=`basename $(git rev-parse --show-toplevel)`
   local branch_name=`git branch | grep \*.* | sed -e 's/\*\ //'`
   local cmd=$(tmux display-message -p "#{pane_current_command}")
 
   local str=
   if [[ $cmd != "ssh" ]] && [[ $cmd != "sftp" ]];then
+    if [[ ! -z ${repo_name} ]];then
+      str+="#[fg=colour166][${repo_name}]"
+    fi
     if [[ ! -z ${branch_name} ]];then
       str+="#[fg=green][${branch_name}]"
     fi
