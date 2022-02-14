1d48afea0344effcff60220d2c43f404c9383cc1
madayo <madayodama@gmail.com>
2022-02-14 14:00:19 +0900

    nvm に n ではなく volta を使用するようにする

--- bin/initialize/node.sh
+++ bin/initialize/node.sh
@@ -7,16 +7,11 @@ source ../functions
 # for windows
 if [[ "$(uname 2> /dev/null)" =~ MSYS ]];then
   # windows 向けのインストーラを使用して、手動でインストールする
-  print_info 'node が必要な場合は https://github.com/coreybutler/nvm-windows を使用して手動でインストールしてください。'
-  print_info 'インストール先、シンボリックリンク先は自由に設定してください。もしパス周りが解決できず、 node --verison の様に使用できない場合は環境変数をいじってください。'
-  print_info 'export PATH=/d/node/nodejs:$PATH  など・・・'
+  print_info 'node が必要な場合は volta https://docs.volta.sh/guide/getting-started を手動でインストールしてください。'
 fi
 
 # for ubuntu
 if [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]];then
-  sudo apt install -y nodejs npm
-  sudo npm install n -g
-  n stable
-  sudo apt purge -y nodejs npm
+  curl https://get.volta.sh | bash
   exec $SHELL -l
 fi
